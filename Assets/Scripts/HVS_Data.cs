f570bf5a826f75c9c7b1ed2e14de6d305d0dfe2c
Andrew Qin <andrewqin29@gmail.com>
2025-07-30 00:32:32 -0700

    new resources

---
+++ Assets/Scripts/HVS_Data.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class HVS_Data
+{
+
+    public string name;    
+    public long sourceId;
+
+    public Vector3 position;
+    public Vector3 velocity;
+
+    public float[,] covarianceMatrix = new float[6, 6];
+
+    public HVS_Data(string hvsName, long id, Vector3 pos, Vector3 vel, float[,] covMatrix)
+    {
+        name = hvsName;
+        sourceId = id;
+        position = pos;
+        velocity = vel;
+        covarianceMatrix = covMatrix;
+    }
+}
